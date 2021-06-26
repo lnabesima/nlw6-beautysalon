55c13ff4a74dac7a12d4c53298038b78ee58527e
Lucas Nabesima <lucas.nabesima@gmail.com>
2021-06-26 18:21:15 -0300

    add responsiveness to large devices

--- main.js
+++ main.js
@@ -39,7 +39,14 @@ const swiper = new Swiper('.swiper-container', {
         el: '.swiper-pagination'
     },
     mousewheel: true,
-    keyboard: true
+    keyboard: true,
+    breakpoints: {
+        767: {
+            slidesPerView: 2,
+            setWrapperSize: true,
+        }
+    }
+
 });
 
 /*scrollreveal*/
